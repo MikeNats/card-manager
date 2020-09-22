7e7d0ea78b8f64851ac0319c4b480b701ef43701
Mike <michail.tsougkranis@gmail.com>
2020-09-22 20:43:26 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import CardContainer from './view/Cards/CardContainer/CardContainer'
+import ErrorBoundary from './view/common/ErrorBoundary/ErrorBoundary'
 
 const App = () =>
-  <section className="app" >
+  <section className="app cas-padding-4" >
+    <React.StrictMode>
+      <ErrorBoundary>
+        <CardContainer/>
+      </ErrorBoundary>
+    </React.StrictMode>
   </section>
 
 export default App
