2231ab66e532ff9ce375235331a0e3a736666aee
Mike <michail.tsougkranis@gmail.com>
2020-09-22 21:42:42 +0100

    class removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CardContainer from './view/Cards/CardContainer/CardContainer'
 import ErrorBoundary from './view/common/ErrorBoundary/ErrorBoundary'
 
 const App = () =>
-  <section className="app cas-padding-4" >
+  <section className="app" >
     <React.StrictMode>
       <ErrorBoundary>
         <CardContainer/>
